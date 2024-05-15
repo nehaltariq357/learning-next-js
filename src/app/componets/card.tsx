250ec64f61d1c1f1be660cb51adfc356908c57f8
nehaltariq357 <nehaltariq78@gmail.com>
2024-05-14 17:34:31 -0800

    first commit

---
+++ src/app/componets/card.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div>card1</div>
+  )
+}
+
+
+
+export const Card2 = () => {
+  return (
+    <div>card2</div>
+  )
+}
+
+
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Card,Card2} from "./componets/card";
+
+const Page = () => {
+  return (
+    <div>
+      <Card />
+      <Card />
+
+    </div>
+  );
+};
+
+export default Page;
